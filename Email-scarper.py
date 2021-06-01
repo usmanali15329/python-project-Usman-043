b0df4ad85a25dfacd9328b04f823dd51c5cb061f
Muhammad Usman Ali <66012883+usmanali15329@users.noreply.github.com>
2021-06-01 12:40:24 +0500

    Add files via upload

---
+++ Email-scarper.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from bs4 import BeautifulSoup
+import requests
+import requests.exceptions
+import urllib.parse
+from collections import deque
+import re
+
+user_url = st.text_input('[+] Enter Target URL To Scan: ')
+urls = deque([user_url])
+
+scraped_urls = set()
+emails = set()
+
+count = 0
+try:
+    while len(urls):
+        count += 1
+        if count == 100:
+            break
+        url = urls.popleft()
+        scraped_urls.add(url)
+
+        parts = urllib.parse.urlsplit(url)
+        base_url = '{0.scheme}://{0.netloc}'.format(parts)
+
+        path = url[:url.rfind('/')+1] if '/' in parts.path else url
+
+        st.write('[%d] Processing %s' % (count, url))
+        try:
+            response = requests.get(url)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+            continue
+
+        new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
+        emails.update(new_emails)
+
+        soup = BeautifulSoup(response.text, features="lxml")
+
+        for anchor in soup.find_all("a"):
+            link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
+            if link.startswith('/'):
+                link = base_url + link
+            elif not link.startswith('http'):
+                link = path + link
+            if not link in urls and not link in scraped_urls:
+                urls.append(link)
+except KeyboardInterrupt:
+    st.write('[-] Closing!')
+
+for mail in emails:
+    st.write(mail)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run Email-scarper.py
